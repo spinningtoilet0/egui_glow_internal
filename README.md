fce24d3dffdb5d4de923845f6862da5e87b49ff8
spinningtoilet0 <nonya@beezwax.com>
2024-01-27 15:29:17 -0500

    whoopsies uploaded wrong version

---
+++ README.md
@@ -0,0 +1,3 @@
+# egui_glow_internal
+An API to use [egui_glow](https://github.com/emilk/egui/tree/master/crates/egui_glow) to render your app given only a window.
+Please note that this is Windows only.
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use clipboard::{windows_clipboard::WindowsClipboardContext, ClipboardProvider};
 use egui::{Event, Key, Modifiers, PointerButton, Pos2, RawInput, Rect, Vec2};
-
-use std::rc::Rc;
+use std::sync::Arc;
 use windows::{
     Wdk::System::SystemInformation::NtQuerySystemTime,
     Win32::{
@@ -99,7 +98,7 @@ pub unsafe fn init(window_handle: HDC) -> Result<(), Error> {
         return Err(Error::CtxSwitch);
     }
 
-    let gl = Rc::new(unsafe {
+    let gl = Arc::new(unsafe {
         egui_glow::glow::Context::from_loader_function_cstr(|s| {
             let result = wglGetProcAddress(windows::core::PCSTR::from_raw(s.as_ptr() as _));
             if result.is_some() {
@@ -370,6 +369,7 @@ pub fn on_event(umsg: u32, wparam: usize, lparam: isize) -> Result<bool, Error> 
                     modifiers,
                     key,
                     repeat: lparam & (KF_REPEAT as isize) > 0,
+                    physical_key: Some(key),
                 });
             }
         }
@@ -383,6 +383,7 @@ pub fn on_event(umsg: u32, wparam: usize, lparam: isize) -> Result<bool, Error> 
                     modifiers,
                     key,
                     repeat: lparam & (KF_REPEAT as isize) > 0,
+                    physical_key: Some(key),
                 });
             }
         }
@@ -438,12 +439,17 @@ fn alter_modifiers(state: &mut EguiState, new: Modifiers) {
     }
 }
 
+/// https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
 fn get_key(wparam: usize) -> Option<Key> {
     match wparam {
-        0x30..=0x39 => unsafe { Some(std::mem::transmute::<_, Key>(wparam as u8 - 0x1F)) },
-        0x41..=0x5A => unsafe { Some(std::mem::transmute::<_, Key>(wparam as u8 - 0x26)) },
-        0x60..=0x69 => unsafe { Some(std::mem::transmute::<_, Key>(wparam as u8 - 0x4F)) },
-        0x70..=0x83 => unsafe { Some(std::mem::transmute::<_, Key>(wparam as u8 - 0x3B)) },
+        // number keys
+        0x30..=0x39 => unsafe { Some(std::mem::transmute::<_, Key>(wparam as u8 - 0x13)) },
+        // letter keys
+        0x41..=0x5A => unsafe { Some(std::mem::transmute::<_, Key>(wparam as u8 - 0x1A)) },
+        // numpad keys
+        0x60..=0x69 => unsafe { Some(std::mem::transmute::<_, Key>(wparam as u8 - 0x43)) },
+        // f1-f20
+        0x70..=0x83 => unsafe { Some(std::mem::transmute::<_, Key>(wparam as u8 - 0x2F)) },
         _ => match VIRTUAL_KEY(wparam as u16) {
             VK_DOWN => Some(Key::ArrowDown),
             VK_LEFT => Some(Key::ArrowLeft),
